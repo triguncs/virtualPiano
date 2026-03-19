2d181d5645bca80e133709a918820cdc41330981
skyluf <trigunpatil07@gmail.com>
2026-03-19 20:01:00 +0530

    Added recording & play recording feature

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+let isRecording = false;
+let recordingStartTime = 0;
+let recordedKeys = [];
+
+const recordBtn = document.querySelector('.js-record');
+const playBtn = document.querySelector('.js-play');
 const piano = document.querySelector('.piano');
+
 piano.addEventListener("mousedown", (event) => {
   const key = event.target.closest(".key");
   if (!key) return;
@@ -9,4 +16,47 @@ piano.addEventListener("mousedown", (event) => {
 function playNote(key) {
   const note = key.classList[2];
   new Audio(`sounds/${note}.mp3`).play();
+
+  if (isRecording) {
+    let timeOffset = Date.now() - recordingStartTime;
+
+    recordedKeys.push({
+      note: note,
+      time: timeOffset
+    });
+  }
 }
+
+function startRecording() {
+  recordedKeys = [];
+  recordingStartTime = Date.now();
+  isRecording = true;
+}
+
+function stopRecording() {
+  isRecording = false;
+}
+
+recordBtn.addEventListener("click", function() {
+  if (!isRecording) {
+    startRecording();
+    recordBtn.textContent = "Stop Recording";
+  } else {
+    stopRecording();
+    recordBtn.textContent = "Start Recording";
+    console.log(recordedKeys);
+  }
+});
+
+function playRecording() {
+  recordedKeys.forEach(function(noteObject) {
+    setTimeout(function() {
+      new Audio(`sounds/${noteObject.note}.mp3`).play();
+    }, noteObject.time);
+  });
+}
+
+playBtn.addEventListener('click', function() {
+  if (recordedKeys.length === 0) return;
+  playRecording();
+});
